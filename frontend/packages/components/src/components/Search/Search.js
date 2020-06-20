0b1d8183232b1f14df34503a4f4d4c4216c71da8
dimad <dima.demyanov.1997@gmail.com>
2020-06-21 02:39:54 +0300

    Added searching bar

--- frontend/packages/components/src/components/Search/Search.js
+++ frontend/packages/components/src/components/Search/Search.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
+import React, {Component} from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    searchBar: {
+      fontSize: 24,
+      width: '100%',
+      height: 50,
+      borderColor: '#DDDDDD',
+      borderWidth: '1px',
+      borderRadius: '10px',
+      backgroundColor: 'white',
+      padding: '10px',
+    },
+  });
 
 export const Search = () => {
     return (
-        <Container>
-            <Header searchBar rounded>
-                <Item>
-                    <Icon name="ios-search" />
-                    <Input placeholder="Search" />
-                    <Icon name="ios-people" />
-                </Item>
-                <Button transparent>
-                    <Text>Search</Text>
-                </Button>
-            </Header>
-        </Container>
+        <TextInput
+        //   onChangeText={/* (search) => this.setState({search}) */}
+          placeholder="Search for ideas and challenges"
+          style={styles.searchBar}
+        />
     );
 }
