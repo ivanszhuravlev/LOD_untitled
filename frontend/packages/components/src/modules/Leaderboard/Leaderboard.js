fdf4f37b8a2ed8b32a2825f6e40437476f3e0d37
dimad <dima.demyanov.1997@gmail.com>
2020-06-20 22:46:36 +0300

    Added block for user page

--- frontend/packages/components/src/modules/Leaderboard/Leaderboard.js
+++ frontend/packages/components/src/modules/Leaderboard/Leaderboard.js
@@ -3,12 +3,15 @@ import { LeaderboardList } from "../LeaderboardList/LeaderboardList";
 import { Block } from "../../components/Block/Block";
 import { Title } from "../../components/Title/Title";
 import { Separator } from "../../components/Separator/Separator";
+import { Card } from "../../components/Card/Card";
 
 export const Leaderboard = () => {
   return (
     <Block>
+      <Card>
       <Title>Leaderboard</Title>
       <Separator />
+      </Card>
       <LeaderboardList />
     </Block>
   );
--- frontend/packages/components/src/modules/User/User.js
+++ frontend/packages/components/src/modules/User/User.js
@@ -2,13 +2,44 @@ import React from "react";
 import { Card } from "../../components/Card/Card";
 import { Separator } from "../../components/Separator/Separator";
 import { Title } from "../../components/Title/Title";
+import ach from "../../assets/achivments.PNG"
+import { Image } from "react-native";
+import styled from "styled-components/native";
+import { View } from "react-native";
+
+const AchImage = styled(Image)`
+  width: 500px;
+  height: 300px;
+  resize-mode: contain;
+`;
+
+const UserContainer = styled(View)`
+  flex-direction: row;
+`;
+
+const UserCard = styled(Card)`
+  flex: 1;
+  heigth: 700px;
+  background-color: #ff00ff;
+`;
+
+const AchivmentCard = styled(Card)`
+  margin-left: 16px;
+`;
 
 export const User = () => {
   return (
-    <Card>
-      <Title>User profile</Title>
-      <Separator/>
-      <UserView/>
-    </Card>
+    <View>
+      <Card>
+        <Title>User profile</Title>
+        <Separator />
+      </Card>
+      <UserContainer>
+        <UserCard />
+        <AchivmentCard>
+          <AchImage source={ach} />
+        </AchivmentCard>
+      </UserContainer>
+    </View>
   );
 };
