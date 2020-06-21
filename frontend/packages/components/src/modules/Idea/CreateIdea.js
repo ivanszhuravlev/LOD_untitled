0d75215b7eac3f3bc7aa7eb72e3e8f6cc3dcb749
David Indzhiev <egniel@yandex.ru>
2020-06-21 08:55:51 +0300

    update craete idea

--- frontend/packages/components/src/modules/Idea/CreateIdea.js
+++ frontend/packages/components/src/modules/Idea/CreateIdea.js
@@ -6,6 +6,7 @@ import { IdeaTop } from "../Idea/IdeaTop";
 import { TextInput } from '../../components/TextInput/TextInput';
 import { View, Button, Text } from 'react-native';
 import { Title } from '../../components/Title/Title';
+import { Tag } from '../../components/Tag/Tag';
 
 export const CreateIdea = ({ idea }) => {
   const Form = styled(Card)`
@@ -13,11 +14,18 @@ export const CreateIdea = ({ idea }) => {
   `
   const FormTitle = styled(Title)`
     // padding-bottom: 20px;
-
   `;
+
+  const CustomTag = styled(Tag)`
+    margin-bottom: 20px;
+  `
 
   const SubmitButton = styled(Button)`
   `;
+  const tag1 = {title: "Программисты"};
+  const tag2 = {title: "Бэкофис на Невском"};
+  const tag3 = {title: "Санкт-Петербург"};
+  const tag4 = {title: "Все офисы"};
 
 
   return (
@@ -26,6 +34,10 @@ export const CreateIdea = ({ idea }) => {
     <Form>
       <TextInput label="Заголовок" placeholder="Введите заголовок"/>
       <TextInput label="Описание" placeholder="Введите описание"/>
+      <CustomTag tag={tag1}></CustomTag>
+      <CustomTag tag={tag2}></CustomTag>
+      <CustomTag tag={tag3}></CustomTag>
+      <CustomTag tag={tag4}></CustomTag>
       <SubmitButton title={"Создать"}></SubmitButton>
     </Form>
     </>
