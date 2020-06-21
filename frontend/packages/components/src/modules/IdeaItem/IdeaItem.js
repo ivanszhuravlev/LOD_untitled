50f08e7e43789f8b6dbaa3509253208b033ef64b
Ivan Zhuravlev <ivan.zhuravlev@noveogroup.com>
2020-06-21 08:28:56 +0300

    Add voting to home

--- frontend/packages/components/src/modules/IdeaItem/IdeaItem.js
+++ frontend/packages/components/src/modules/IdeaItem/IdeaItem.js
@@ -9,11 +9,12 @@ import { TagsList } from "../TagsList/TagsList";
 import { getReadableTime } from "../../utils/getReadableTime";
 import { UserDateBlock } from "./UserDateBlock";
 import { IdeaStatus } from "./IdeaStatus";
+import { Vote } from "../../components/Vote/Vote";
 
 const IdeaCard = styled(Card)`
-  flex-direction: column;
-  align-items: flex-start;
-  padding: ${({ theme }) => theme.paddingMedium}px;
+  flex-direction: row;
+  justify-content: flex-start;
+  box-sizing: border-box;
   margin: 0;
 `;
 
@@ -49,23 +50,49 @@ const Description = styled(Text)`
   margin: ${({ theme }) => theme.paddingTiny2}px 0px;
 `;
 
+const LeftSide = styled(View)`
+  align-items: flex-start;
+`;
+
+const RightSide = styled(View)`
+  flex-direction: column;
+  align-items: flex-start;
+  padding: ${({ theme }) => theme.paddingMedium}px;
+  box-sizing: border-box;
+  flex: 1;
+`;
+
+const VoteStyled = styled(Vote)`
+  flex-direction: column;
+  height: 160px;
+  width: 36px;
+  margin: 0;
+  /* padding: 0; */
+  box-sizing: border-box;
+`;
+
 export const IdeaItem = ({ idea, ...props }) => {
   const { tags, title, id, created_at, description, status } = idea;
   const href = getLink(Screens.Idea, { id });
   console.log("idea", idea);
   return (
     <IdeaCard {...props}>
-      <Link href={href}>
-        <IdeaTitle>{title}</IdeaTitle>
-      </Link>
-      <Description>{description}</Description>
-      <TagsContainer>
-        <TagsList tags={tags} />
-      </TagsContainer>
-      <InfoContainer>
-        <UserDateBlock date={getReadableTime(created_at)} />
-        <IdeaStatus status={status} />
-      </InfoContainer>
+      <LeftSide>
+        <VoteStyled rating={0} />
+      </LeftSide>
+      <RightSide>
+        <Link href={href}>
+          <IdeaTitle>{title}</IdeaTitle>
+        </Link>
+        <Description>{description}</Description>
+        <TagsContainer>
+          <TagsList tags={tags} />
+        </TagsContainer>
+        <InfoContainer>
+          <UserDateBlock date={getReadableTime(created_at)} />
+          <IdeaStatus status={status} />
+        </InfoContainer>
+      </RightSide>
     </IdeaCard>
   );
 };
